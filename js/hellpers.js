0c8196a184910a16335a80f0a003db74fba8fa17
Leanbader <baderleandro@gmail.com>
2024-01-08 01:10:02 -0300

    Se completo la lectura de los productos en la pagina de administracion

--- js/hellpers.js
+++ js/hellpers.js
@@ -50,3 +50,45 @@ export function validarTodo(inpCodigo,inpNombre,inpDescripcion,inpPrecio,inpUrl)
         return false;
     }
 }
+
+//ALAN NO LO PUDO HACER EN CLASE, LO HICE YO LUEGO Y ESTABA FUNCIONANDO
+
+//  export function CodigoAleatorio(){
+//      const regCode=JSON.parse(localStorage.getItem("regCode")) || [];
+//      let repeat = false;
+//      let code;
+//      do {
+//          if(regCode.length>=10){
+//              console.log("No quedan mas digitos por generar...");
+//              break
+//          }
+//          code = parseInt(Math.random()*10);
+//          for(let i = 0; i < regCode.length; i++){
+//              if (code === regCode[i]) {
+//                  repeat = true;
+//                  break
+//              }else{
+//                  repeat = false;
+//              }
+//          }
+//      } while (repeat);
+  
+//      if (regCode.length<10) {
+//          regCode.push(code);
+//          localStorage.setItem("regCode",JSON.stringify(regCode));
+//          return code;
+//      }else{
+//          return code;
+//      }
+    
+//      // regCode.push(code);
+//      // localStorage.setItem("regCode",JSON.stringify(regCode));
+//      // return code;
+//  }
+
+export function ObtenerCodigoAleatorio(){
+     return window.crypto.randomUUID()//De esta manera nos genera codigos unicos es parte del DOM
+ }
+
+ 
+
